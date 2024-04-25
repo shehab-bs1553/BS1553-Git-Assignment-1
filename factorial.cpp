7653d416d0422578f096f4a9fb95b39882793b84
Md shehab Hossain <shehab.hossain@brainstation-23.com>
2024-04-25 11:06:41 +0600

    Add a new file factorial.cpp

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int number;
+    cin>>number;
+    int sum=0;
+    for(int i=1; i<=number; i++)
+    {
+        sum*=i;
+    }
+    cout<<sum<<endl;
+}
